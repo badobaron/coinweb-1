b235df96ee4aab56bdc782214210a483e8bb0e95
Bhaintoo Apps <rajattyaagii@gmail.com>
2018-02-17 18:26:03 +0530

    First commit

--- coinapp/views.py
+++ coinapp/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-	return render(request, 'coinapp/index.html', {})
+	return render(request, 'coinapp/index2.html', {})
 
 
 def uc_btc_by(request):
